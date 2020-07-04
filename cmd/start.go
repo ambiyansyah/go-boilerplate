66accfbd05c34bfaa014e27c573e92393e046462
Risyal Ambiyansyah <ambiyansyah.risyal@gmail.com>
2020-07-04 15:52:13 +0700

    initial code

---
+++ cmd/start.go
@@ -0,0 +1,23 @@
+package cmd
+
+import (
+	"fmt"
+
+	"github.com/spf13/cobra"
+)
+
+// startCmd represents the start command
+var startCmd = &cobra.Command{
+	Use:   "start",
+	Short: "start app",
+	Long:  "start app",
+	Run: func(cmd *cobra.Command, args []string) {
+		fmt.Println("start called")
+	},
+}
+
+func init() {
+	rootCmd.AddCommand(startCmd)
+	startCmd.Flags().String("type", "http", "http or grpc")
+	startCmd.MarkFlagRequired("type")
+}
